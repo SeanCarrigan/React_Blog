ab79373d3556dd72c89547387b8b45cc5a3a5af0
SeanCarrigan <112054424+SeanCarrigan@users.noreply.github.com>
2024-02-06 15:52:58 -0600

    removed DataContext and add Redux

--- react_blog/src/components/EditPost.js
+++ react_blog/src/components/EditPost.js
@@ -1,18 +1,24 @@
 import { useEffect, useState, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import { format } from "date-fns";
-import api from "./api/posts";
 import { useNavigate } from "react-router-dom";
 
-import DataContext from "./context/DataContext";
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 const EditPost = () => {
-  const { posts, setPosts } = useContext(DataContext);
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
-  const { id } = useParams();
-  const post = posts.find((post) => post.id.toString() === id);
   const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const editTitle = useStoreState((state) => state.editTitle);
+  const editBody = useStoreState((state) => state.editBody);
+
+  const editPost = useStoreActions((actions) => actions.editPost);
+  const setEditTitle = useStoreActions((actions) => actions.setEditTitle);
+  const setEditBody = useStoreActions((actions) => actions.setEditBody);
+
+  const getPostById = useStoreState((state) => state.getPostById);
+  const post = getPostById(id);
 
   useEffect(() => {
     if (post) {
@@ -21,26 +27,11 @@ const EditPost = () => {
     }
   }, [post, setEditTitle, setEditBody]);
 
-  const handleEdit = async (id) => {
+  const handleEdit = (id) => {
     const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    // create a new post object
-    const updatedPost = {
-      id,
-      title: editTitle,
-      datetime,
-      body: editBody,
-    };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditTitle("");
-      setEditBody("");
-      navigate("/");
-    } catch (err) {
-      console.log("Error", err.message);
-    }
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    editPost(updatedPost);
+    navigate(`/post/${id}`);
   };
 
   return (
@@ -64,7 +55,7 @@ const EditPost = () => {
               value={editBody}
               onChange={(e) => setEditBody(e.target.value)}
             />
-            <button type="submit" onClick={() => handleEdit(post.id)}>
+            <button type="button" onClick={() => handleEdit(post.id)}>
               Submit
             </button>
           </form>
@@ -73,6 +64,7 @@ const EditPost = () => {
       {!editTitle && (
         <>
           <h2>Post Not Found</h2>
+          <p>Well, that's disappointing.</p>
           <p>
             <Link to="/">Visit Our Homepage</Link>
           </p>
--- react_blog/src/index.js
+++ react_blog/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { StoreProvider } from "easy-peasy";
+import store from "./components/store";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
